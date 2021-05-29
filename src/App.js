c54d5cd261f1b7777b01009854f4d9353b2b3c13
SvonkLoki <bot_on_war3@mail.ru>
2021-05-29 20:43:17 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { Route } from "react-router";
-
-<<<<<<< HEAD
+import {Route} from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import Profile from "./Components/Profile";
-=======
-const App = () => {
-    return (
-        <div className='app'>
-            <Header/>
-            <Profile/>
-            <div className='app-content'>
-                <Repositories/>
-            </div>
->>>>>>> 3eeb1394a8f69b6da595ca078ac82a2c8964bbd5
 
 const App = (props) => {
-  return (
-    <div className="app">
-      <Header />
-      <div className="container">
-        <Route path="/:login/:pageNumber?">
-          <Profile user={props.user} />
-        </Route>
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header/>
+            <div className="container">
+                <Route path="/:login/:pageNumber?">
+                    <Profile />
+                </Route>
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
-import store from "./redux/store";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import store from './redux/store';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from 'react-redux'
 
-ReactDOM.render(<
-        BrowserRouter>
-        <
-            Provider store={store}>
-            <
-                App / >
-        <
-    /Provider> <
-    /BrowserRouter>,
-    document.getElementById('root')
-);
->>>>>>> 3eeb1394a8f69b6da595ca078ac82a2c8964bbd5
